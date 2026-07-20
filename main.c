f230aa94aae009d0553ae6a29510f5711a4d3f6a
chamajayakodi <chamathsarajayakodi@gmail.com>
2026-07-20 11:16:12 +0530

    Added showtimes and seat map

--- main.c
+++ main.c
@@ -4,6 +4,28 @@ int main()
 {
     int choice;
 
+    // Showtimes
+    char showtimes[][50] =
+    {
+        "10:00 AM - Avatar 2",
+        "01:00 PM - Room No. 106",
+        "04:00 PM - Toy Story 5",
+        "07:00 PM - Minions",
+        "10:00 PM - Spider-Man"
+    };
+
+    int totalShows = 5;
+
+    // Seat Map (O = Available, X = Booked)
+    char seats[5][5] =
+    {
+        {'O','O','O','O','O'},
+        {'O','O','X','O','O'},
+        {'O','O','O','O','O'},
+        {'X','O','O','O','X'},
+        {'O','O','X','X','O'}
+    };
+
     do
     {
         printf("\n=====================================\n");
@@ -23,39 +45,63 @@ int main()
         switch(choice)
         {
             case 1:
-                printf("View Showtimes selected.\n");
+            {
+                printf("\n========== TODAY'S SHOWTIMES ==========\n");
+
+                for(int i = 0; i < totalShows; i++)
+                {
+                    printf("%d. %s\n", i + 1, showtimes[i]);
+                }
+
                 break;
+            }
 
             case 2:
-                printf("View Seat Map selected.\n");
+            {
+                printf("\n========== SEAT MAP ==========\n\n");
+
+                printf("      1 2 3 4 5\n");
+
+                for(int i = 0; i < 5; i++)
+                {
+                    printf("Row %d ", i + 1);
+
+                    for(int j = 0; j < 5; j++)
+                    {
+                        printf("%c ", seats[i][j]);
+                    }
+
+                    printf("\n");
+                }
+
                 break;
+            }
 
             case 3:
-                printf("Book a Seat selected.\n");
+                printf("\nBook a Seat selected.\n");
                 break;
 
             case 4:
-                printf("Cancel Booking selected.\n");
+                printf("\nCancel Booking selected.\n");
                 break;
 
             case 5:
-                printf("Search Booking selected.\n");
+                printf("\nSearch Booking selected.\n");
                 break;
 
             case 6:
-                printf("Revenue Report selected.\n");
+                printf("\nRevenue Report selected.\n");
                 break;
 
             case 7:
-                printf("Thank you for using the system!\n");
+                printf("\nThank you for using the Movie Ticket Booking System!\n");
                 break;
 
             default:
-                printf("Invalid choice! Please try again.\n");
+                printf("\nInvalid choice! Please try again.\n");
         }
 
     } while(choice != 7);
 
     return 0;
 }
-
