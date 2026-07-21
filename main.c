7457cc6fe812ee3f8594b79112cf4ceb5219b3f1
chamajayakodi <chamathsarajayakodi@gmail.com>
2026-07-22 00:56:01 +0530

    Editted showtimes and seat map 2026.07.22

--- main.c
+++ main.c
@@ -4,27 +4,43 @@ int main()
 {
     int choice;
 
-    // Showtimes
-    char showtimes[][50] =
+    // 5 Movies
+    char movies[5][30] =
     {
-        "10:00 AM - Avatar 2",
-        "01:00 PM - Room No. 106",
-        "04:00 PM - Toy Story 5",
-        "07:00 PM - Minions",
-        "10:00 PM - Spider-Man"
+        "Avatar 2",
+        "Room No. 106",
+        "Toy Story 5",
+        "Minions",
+        "Spider-Man"
     };
 
-    int totalShows = 5;
-
-    // Seat Map (O = Available, X = Booked)
-    char seats[5][5] =
+    // 2 Showtimes for each movie
+    char showtimes[5][2][20] =
     {
-        {'O','O','O','O','O'},
-        {'O','O','X','O','O'},
-        {'O','O','O','O','O'},
-        {'X','O','O','O','X'},
-        {'O','O','X','X','O'}
+        {"10:00 AM", "07:00 PM"},
+        {"11:00 AM", "08:00 PM"},
+        {"12:00 PM", "09:00 PM"},
+        {"01:00 PM", "10:00 PM"},
+        {"02:00 PM", "11:00 PM"}
     };
+
+    // 5 Movies × 2 Showtimes × 5 Rows × 10 Seats
+    char seats[5][2][5][10];
+
+    // Initialize all seats as available
+    for(int m = 0; m < 5; m++)
+    {
+        for(int s = 0; s < 2; s++)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    seats[m][s][i][j] = '.';
+                }
+            }
+        }
+    }
 
     do
     {
@@ -46,11 +62,16 @@ int main()
         {
             case 1:
             {
-                printf("\n========== TODAY'S SHOWTIMES ==========\n");
+                printf("\n========== MOVIES & SHOWTIMES ==========\n");
 
-                for(int i = 0; i < totalShows; i++)
+                for(int i = 0; i < 5; i++)
                 {
-                    printf("%d. %s\n", i + 1, showtimes[i]);
+                    printf("\nMovie %d: %s\n", i + 1, movies[i]);
+
+                    for(int j = 0; j < 2; j++)
+                    {
+                        printf("   Showtime %d: %s\n", j + 1, showtimes[i][j]);
+                    }
                 }
 
                 break;
@@ -58,18 +79,68 @@ int main()
 
             case 2:
             {
-                printf("\n========== SEAT MAP ==========\n\n");
+                int movieChoice, showChoice;
 
-                printf("      1 2 3 4 5\n");
+                printf("\nSelect Movie:\n");
 
                 for(int i = 0; i < 5; i++)
                 {
-                    printf("Row %d ", i + 1);
+                    printf("%d. %s\n", i + 1, movies[i]);
+                }
 
-                    for(int j = 0; j < 5; j++)
+                printf("Enter Movie Number: ");
+                scanf("%d", &movieChoice);
+
+                if(movieChoice < 1 || movieChoice > 5)
+                {
+                    printf("Invalid movie!\n");
+                    break;
+                }
+
+                printf("\nSelect Showtime:\n");
+
+                for(int i = 0; i < 2; i++)
+                {
+                    printf("%d. %s\n", i + 1, showtimes[movieChoice - 1][i]);
+                }
+
+                printf("Enter Showtime Number: ");
+                scanf("%d", &showChoice);
+
+                if(showChoice < 1 || showChoice > 2)
+                {
+                    printf("Invalid showtime!\n");
+                    break;
+                }
+
+                printf("\n========== SEAT MAP ==========\n\n");
+
+                printf("      ");
+
+                for(int j = 1; j <= 10; j++)
+                {
+                    printf("%2d ", j);
+                }
+
+                printf("\n");
+
+                char rowNames[5] = {'A', 'B', 'C', 'D', 'E'};
+
+                for(int i = 0; i < 5; i++)
+                {
+                    printf("%c     ", rowNames[i]);
+
+                    for(int j = 0; j < 10; j++)
                     {
-                        printf("%c ", seats[i][j]);
+                        printf("%c  ", seats[movieChoice - 1][showChoice - 1][i][j]);
                     }
+
+                    if(i == 0 || i == 1)
+                        printf(" Regular (Rs.500)");
+                    else if(i == 2 || i == 3)
+                        printf(" Premium (Rs.750)");
+                    else
+                        printf(" VIP (Rs.1000)");
 
                     printf("\n");
                 }
